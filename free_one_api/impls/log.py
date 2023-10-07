7cb9fcefb3a4c6607de3ba6b5e81cb0f00869560
Junyan Qin <1010553892@qq.com>
2023-10-07 01:16:52 +0000

    feat(log): make db log handler a producer-consumer queue

--- free_one_api/impls/log.py
+++ free_one_api/impls/log.py
@@ -8,13 +8,25 @@ class SQLiteHandler(logging.Handler):
     """SQLite logging handler."""
 
     dbmgr: db.DatabaseInterface
+    
+    queue: asyncio.Queue = None
 
     def __init__(self, dbmgr: db.DatabaseInterface, level=logging.NOTSET):
         super().__init__(level)
         self.dbmgr = dbmgr
+        
+    async def _consumer(self):
+        while True:
+            record: logging.LogRecord = await self.queue.get()
+            await self.dbmgr.insert_log(record.created, record.getMessage())
+            self.queue.task_done()
 
     def emit(self, record: logging.LogRecord):
         """Emit a record."""
         loop = asyncio.get_running_loop()
         
-        loop.create_task(self.dbmgr.insert_log(record.created, record.getMessage()))
+        if self.queue is None:
+            self.queue = asyncio.Queue()
+            loop.create_task(self._consumer())
+            
+        loop.create_task(self.queue.put(record))
