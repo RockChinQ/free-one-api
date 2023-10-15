768c60e880bb8b391064a7459cd6fba93eb22572
Junyan Qin <1010553892@qq.com>
2023-10-15 02:07:18 +0000

    perf: load balance

--- free_one_api/impls/channel/eval.py
+++ free_one_api/impls/channel/eval.py
@@ -1,4 +1,5 @@
 import random
+import math
 import time
 
 from ...models.channel import evaluation
@@ -26,24 +27,28 @@ class ChannelEvaluation(evaluation.AbsChannelEvaluation):
         
         lastUseTime = -1
         
-        last5Records: list[evaluation.Record] = []
-        
         if len(records_reverse) == 0:
             lastUseTime = now_time - self.init_time
+
+        using_amount = 0
         
         for record in records_reverse:
             if lastUseTime == -1:
                 if record.end_time < 0:  # querying
                     lastUseTime = 0
+                    using_amount += 1
                 else:  # last query committed
                     lastUseTime = now_time - record.end_time
+            else:
+                if record.end_time < 0:
+                    using_amount += 1
 
-            if len(last5Records) >= 5:
-                break
-            if record.end_time > 0:  # committed
-                if record.success and record.stream:
-                    last5Records.append(record)
+            # if len(last5Records) >= 5:
+            #     break
+            # if record.end_time > 0:  # committed
+            #     if record.success and record.stream:
+            #         last5Records.append(record)
 
-        last5StreamRecordsAverageLatency = sum([record.latency for record in last5Records]) / len(last5Records) if len(last5Records) > 0 else 0
+        # last5StreamRecordsAverageLatency = sum([record.latency for record in last5Records]) / len(last5Records) if len(last5Records) > 0 else 0
         
-        return (0 - last5StreamRecordsAverageLatency) + lastUseTime
+        return round(lastUseTime / 5) * 5 - using_amount * 5
